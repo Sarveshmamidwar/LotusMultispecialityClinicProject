9475a9fd6a1abf1b05e4e92b41bdc40857bcf32e
Sarveshmamidwar <sarvesh.mamidwar21@gmail.com>
2025-02-20 20:03:22 +0530

    20-02-2025-Sarvesh-Apply-Security

---
+++ src/main/java/com/Hospital/config/CustomSecurityConfigurer.java
@@ -0,0 +1,18 @@
+package com.Hospital.config;
+
+import java.io.IOException;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CustomSecurityConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+	
+}
---
+++ src/main/java/com/Hospital/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.Hospital.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/doctors/**").hasRole("DOCTOR")
+                .requestMatchers("/", "/about", "/ouserService","/ouserDoctors","/contacts","/appointment","/bookAppointment","/CSS/**", "/js/**","/images/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                //.loginPage("/login")
+                .defaultSuccessUrl("/doctors/dashboard", true)
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll()
+            );
+            
+        // Apply your custom configurer using apply() method
+        http.apply(new CustomSecurityConfigurer());
+        
+        return http.build();
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
