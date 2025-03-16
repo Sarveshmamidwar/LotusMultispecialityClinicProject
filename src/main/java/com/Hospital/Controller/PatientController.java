c2f5ab982a0ef150b8ac1d94b908e69aeb2fd434
Sarveshmamidwar <sarvesh.mamidwar21@gmail.com>
2025-03-16 19:33:38 +0530

    16-03-2025-Sarvesh-patient-Reports-UI-Correction

--- src/main/java/com/Hospital/Controller/PatientController.java
+++ src/main/java/com/Hospital/Controller/PatientController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Hospital.entity.Patient;
 import com.Hospital.entity.Reports;
@@ -70,4 +73,6 @@ public class PatientController {
 	    model.addAttribute("PatientName",patient.getName());
 	    return "Patient/Reports"; // Ensure this matches your template filename
 	}
+	
+	
 }
